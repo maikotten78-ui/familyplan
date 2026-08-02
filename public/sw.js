71b93716e3500fd4f75905ef269c22b63abbbabf
Maik Otten <maik.ot>
2026-08-02 06:33:10 +0000

    Service Worker: Cache-Version v27 -> v28 (verstärkte Schatten)

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // famiplan Service Worker
-const CACHE_NAME = 'famiplan-v27';
+const CACHE_NAME = 'famiplan-v28';
 
 // App-Shell Assets die beim Install gecacht werden
 const PRECACHE_URLS = ['/', '/index.html', '/icon-192.png', '/icon-512.png'];
