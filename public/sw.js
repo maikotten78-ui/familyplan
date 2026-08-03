486f53b8868f7624beaf5c84ed8bda478fcbedd2
Maik Otten <maik.ot>
2026-08-03 15:18:34 +0000

    Service Worker: Cache-Version v29 -> v30 (Registrierungsformular-Fix)

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // famiplan Service Worker
-const CACHE_NAME = 'famiplan-v29';
+const CACHE_NAME = 'famiplan-v30';
 
 // App-Shell Assets die beim Install gecacht werden
 const PRECACHE_URLS = ['/', '/index.html', '/icon-192.png', '/icon-512.png'];
